6ad5ba5c05ffaedb7d59a61a9509503a512621ca
Leonardo Borges Alves <69164929+Leonarddo-alves@users.noreply.github.com>
2026-06-30 19:47:13 -0300

    Modify sales query order month

--- queries/05_sales_by_month.sql
+++ queries/05_sales_by_month.sql
@@ -1,6 +1,6 @@
 SELECT
-    Segment,
+    strftime('%Y-%m', Order_Date) AS order_month,
     SUM(Sales) AS total_sales
 FROM sales
-GROUP BY Segment
-ORDER BY total_sales DESC;
+GROUP BY order_month
+ORDER BY order_month;
